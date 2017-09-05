385b6251c8cd0fc6b8542b5baef4ca29bc057197
paulmillen <pwmillen@gmail.com>
2017-09-05 19:16:31 +0100

    added Bank and moved module imports to app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var moment = require('moment');
+var CurrentAccount = require('./lib/CurrentAccount.js');
+var Transaction = require('./lib/Transaction.js');
+var Printer = require('./lib/Printer.js');
+var Bank = require('./lib/Bank.js');
