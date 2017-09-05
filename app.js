ff4f58bfa407d1fa8de26c1ad69286fd2b501c17
paulmillen <pwmillen@gmail.com>
2017-09-05 19:34:32 +0100

    refactored bank

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ var CurrentAccount = require('./lib/CurrentAccount.js');
 var Transaction = require('./lib/Transaction.js');
 var Printer = require('./lib/Printer.js');
 var Bank = require('./lib/Bank.js');
+
+bank = new Bank
