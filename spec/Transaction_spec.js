87946e42373850372495c5b41a1a10983ac6507f
paulmillen <pwmillen@gmail.com>
2017-09-05 17:16:33 +0100

    added getters for Transaction properties

--- spec/Transaction_spec.js
+++ spec/Transaction_spec.js
@@ -3,18 +3,34 @@ describe('Transaction', () => {
   var Transaction = require('../lib/Transaction.js')
 
   let date = '01/01/2017'
-  var transaction = new Transaction(date);
-
-  console.log(transaction)
+  let amount = 1000
+  let balance = 1500
+  var transaction = new Transaction(date, amount, balance);
 
   it('instantiates', () => {
     expect(transaction).toBeDefined();
   });
 
   describe('#returnDate', () => {
+
     it('returns the instantiated date object', () => {
       expect(transaction.returnDate()).toEqual(date);
     });
   });
+
+  describe('#returnAmount', () => {
+
+    it('returns the instantiated date object', () => {
+      expect(transaction.returnAmount()).toEqual(amount);
+    });
+  });
+
+  describe('#returnBalance', () => {
+
+    it('returns the instantiated date object', () => {
+      expect(transaction.returnBalance()).toEqual(balance);
+    });
+  });
+
 
 });
