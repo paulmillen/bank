d9b245f70ee06bdd38cb905d0bdcbc18ba97d992
paulmillen <pwmillen@gmail.com>
2017-09-05 18:29:56 +0100

    added Printer#statement and test for larger array in #_transactionLines

--- spec/Printer_spec.js
+++ spec/Printer_spec.js
@@ -15,7 +15,7 @@ describe('Printer', () => {
 
   describe('#_header', () => {
 
-    it('can print four columns', () => {
+    it('returns four column string', () => {
       expect(printer._header()).toEqual('date || credit || debit || balance')
     });
   });
@@ -36,6 +36,35 @@ describe('Printer', () => {
       var printer = new Printer([transaction]);
 
       expect(printer._transactionLines()).toEqual(['01/01/2017 || || 1000 || 1500'])
+    });
+
+    it('prints multiple transactions', () => {
+
+      let transactionOne = {  returnDate()    { return '01/01/2017' },
+                              returnAmount()  { return 1000 },
+                              returnBalance() { return 1500 }
+                        }
+      let transactionTwo =  { returnDate()    { return '02/01/2017' },
+                              returnAmount()  { return -1000 },
+                              returnBalance() { return 500 }
+                            }
+      let transactionThree =  {  returnDate()   { return '04/01/2017' },
+                                returnAmount()  { return 5000 },
+                                returnBalance() { return 5500 }
+                              }
+
+      var printer = new Printer([transactionOne, transactionTwo, transactionThree]);
+
+      expect(printer._transactionLines()).toEqual(['01/01/2017 || 1000 || || 1500', '02/01/2017 || || 1000 || 500', '04/01/2017 || 5000 || || 5500' ])
+    });
+  });
+
+  describe('#statement', () => {
+
+    it('prints a header', () => {
+      console.log = jasmine.createSpy('log')
+      printer.statement();
+      expect(console.log).toHaveBeenCalledWith(printer._header())
     });
   });
 
